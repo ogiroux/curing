39ee4542b80efce890afe23cb676b0e8575c1f9b
Olivier Giroux <ogiroux@gmail.com>
2020-05-29 14:14:12 -0700

    Snapshot

--- curing.cu
+++ curing.cu
@@ -1,3 +1,5 @@
+#include <map>
+#include <vector>
 #include <atomic>
 #include <chrono>
 #include <thread>
@@ -13,6 +15,7 @@
 #include "syscall.h"
 
 #include <cuda/std/atomic>
+#include <cuda/std/semaphore>
 
 # define check(ans) { assert_((ans), __FILE__, __LINE__); }
 inline void assert_(cudaError_t code, const char *file, int line) {
@@ -41,151 +44,259 @@ void run(F f)
     check(cudaFree(_f));
 }
 
-struct app_sq_ring {
-    unsigned *head;
-    unsigned *tail;
-    unsigned *ring_mask;
-    unsigned *flags;
-    unsigned *array;
-};
+namespace cuda
+{
+    struct app_sq_ring {
+        unsigned *head;
+        unsigned *tail;
+        unsigned *ring_mask;
+        unsigned *flags;
+        unsigned *array;
+    };
 
-struct app_cq_ring {
-    unsigned *head;
-    unsigned *tail;
-    unsigned *ring_mask;
-    io_uring_cqe *array;
-};
+    struct app_cq_ring {http://cuda-repo/release-candidates/kitpicks/cuda-r11-0/11.0.1/023/local_installers/cuda_11.0.1_450.36.04_linux_ppc64le.run
+        unsigned *head;
+        unsigned *tail;
+        unsigned *ring_mask;
+        io_uring_cqe *array;
+    };
+    
+    __managed__ int ioring_fd;
+    __managed__ app_sq_ring request;
+    __managed__ app_cq_ring response;
+    __managed__ io_uring_sqe* sqentriesptr;
+    __managed__ iovec vec;
+//    __managed__ std::binary_semaphore lock;
+
+    struct syscall_tunnel
+    {
+        std::atomic_bool done = {false};
+
+        ::std::thread helper;
+
+        syscall_tunnel()
+        {
+            io_uring_params p;
+            ::std::memset(&p, 0, sizeof(p));
+
+            p.flags |= IORING_SETUP_SQPOLL;
+            p.sq_thread_idle = 1000000;
+
+            ioring_fd = __sys_io_uring_setup(ioring_size, &p);
+            if(ioring_fd < 0) {
+                ::std::cout << "Error in setup : " << errno << ::std::endl;
+                abort();
+            }
+
+            check(cudaGetLastError());
+            auto const sqptr = (__u8*)mmap(0, p.sq_off.array + p.sq_entries * sizeof(__u32),
+                                        PROT_READ|PROT_WRITE, MAP_SHARED|MAP_POPULATE,
+                                        ioring_fd, IORING_OFF_SQ_RING);
+            assert(sqptr != MAP_FAILED);
+        //    check(cudaHostRegister(sqptr, p.sq_off.array + p.sq_entries * sizeof(__u32), cudaHostRegisterDefault));
+            request = app_sq_ring{
+                (unsigned *)(sqptr + p.sq_off.head),
+                (unsigned *)(sqptr + p.sq_off.tail),
+                (unsigned *)(sqptr + p.sq_off.ring_mask),
+                (unsigned *)(sqptr + p.sq_off.flags),
+                (unsigned *)(sqptr + p.sq_off.array)
+            };
+
+            sqentriesptr = (io_uring_sqe*)mmap(0, p.sq_entries * sizeof(io_uring_sqe),
+                                        PROT_READ|PROT_WRITE, MAP_SHARED|MAP_POPULATE,
+                                        ioring_fd, IORING_OFF_SQES);
+            assert(sqentriesptr != MAP_FAILED);
+        //    check(cudaHostRegister(sqentriesptr, p.sq_entries * sizeof(io_uring_sqe), cudaHostRegisterDefault));
+
+            auto const cqptr = (__u8*)mmap(0, p.cq_off.cqes + p.cq_entries * sizeof(io_uring_cqe),
+                                PROT_READ|PROT_WRITE, MAP_SHARED|MAP_POPULATE, ioring_fd,
+                                IORING_OFF_CQ_RING);
+            assert(cqptr != MAP_FAILED);
+        //    check(cudaHostRegister(cqptr, p.cq_off.cqes + p.cq_entries * sizeof(io_uring_cqe), cudaHostRegisterDefault));
+            response = app_cq_ring{
+                (unsigned *)(cqptr + p.cq_off.head),
+                (unsigned *)(cqptr + p.cq_off.tail),
+                (unsigned *)(cqptr + p.cq_off.ring_mask),
+                (io_uring_cqe *)(cqptr + p.cq_off.cqes)
+            };
+
+            helper = ::std::thread([&](){
+                while(!done) {
+                    if (reinterpret_cast<std::atomic_uint&>(*request.flags) & IORING_SQ_NEED_WAKEUP)
+                        __sys_io_uring_enter(ioring_fd, 1, 0, IORING_ENTER_SQ_WAKEUP, NULL);
+                    ::std::this_thread::sleep_for(::std::chrono::milliseconds(10));
+                }
+            });
+        }
+
+        ~syscall_tunnel()
+        {
+            done = true;
+            helper.join();
+            close(ioring_fd);
+        }
+
+        ::std::map<int, int> fds;
+        ::std::vector<int> registry;
+    };
+
+    syscall_tunnel t;
+
+    struct FILE
+    {
+        int fd;
+        size_t read_off;
+        size_t write_off;
+    };
+
+    int open(char const* name, int opt)
+    {
+        int const _infd = ::open(name, opt);
+        assert(_infd > 0);
+
+        if(!t.registry.empty())
+            __sys_io_uring_register(ioring_fd,  IORING_UNREGISTER_FILES, 0, 0);
+
+        int const infd = t.registry.size();
+        t.registry.push_back(_infd);
+
+        int const ret = __sys_io_uring_register(ioring_fd,  IORING_REGISTER_FILES, t.registry.data(), t.registry.size());
+        assert(ret == 0);
+
+        t.fds[infd] = _infd;
+        return infd;
+    }
+
+    void close(int infd)
+    {
+        int _infd = t.fds[infd];
+        //__sys_io_uring_register(ioring_fd,  IORING_UNREGISTER_FILES, &_infd, 1);
+        ::close(_infd);
+    }
+
+    __host__ __device__ size_t fread(void * ptr, size_t size, size_t count, FILE * stream) {
+//        lock.acquire();
+        // uring request
+        {
+            ::std::memset(sqentriesptr+0, 0, sizeof(io_uring_sqe));
+            sqentriesptr[0].fd = stream->fd;
+            sqentriesptr[0].opcode = IORING_OP_READV;
+            vec = iovec{ ptr, size * count };
+            sqentriesptr[0].addr = (uint64_t)&vec;
+            sqentriesptr[0].len = 1;
+            sqentriesptr[0].flags = IOSQE_FIXED_FILE;
+            sqentriesptr[0].off = stream->read_off;
+            unsigned const tail = reinterpret_cast<std::atomic_uint&>(*request.tail);
+            while(1)
+            {
+                unsigned const head = reinterpret_cast<std::atomic_uint&>(*request.head);
+                if(head != ((tail + 1) ))
+                    break;
+            }
+            request.array[tail & *request.ring_mask] = 0;
+            reinterpret_cast<std::atomic_uint&>(*request.tail) = (tail + 1);
+        }
+        // uring response
+        {
+            unsigned const head = reinterpret_cast<std::atomic_uint&>(*response.head);
+            while(1)
+            {
+                unsigned const tail = reinterpret_cast<std::atomic_uint&>(*response.tail);
+                if(head != tail)
+                    break;
+            }
+            if(response.array[head & *response.ring_mask].res < 0) {
+                assert(0);
+            }
+            reinterpret_cast<std::atomic_uint&>(*response.head) = (head + 1);
+        }
+        stream->read_off += size * count;
+//        lock.release();
+        return count;
+    };
+
+
+
+    __host__ size_t fwrite(void const * ptr, size_t size, size_t count, FILE * stream) {
+//        lock.acquire();
+        // uring request
+        {
+            ::std::memset(sqentriesptr+0, 0, sizeof(io_uring_sqe));
+            sqentriesptr[0].fd = stream->fd;
+            sqentriesptr[0].opcode = IORING_OP_WRITEV;
+            vec = iovec{ (void*)ptr, size * count };
+            sqentriesptr[0].addr = (uint64_t)&vec;
+            sqentriesptr[0].len = 1;
+            sqentriesptr[0].flags = IOSQE_FIXED_FILE;
+            sqentriesptr[0].off = stream->write_off;
+            unsigned const tail = reinterpret_cast<std::atomic_uint&>(*request.tail);
+            while(1)
+            {
+                unsigned const head = reinterpret_cast<std::atomic_uint&>(*request.head);
+                if(head != ((tail + 1) ))
+                    break;
+            }
+            request.array[tail & *request.ring_mask] = 0;
+            reinterpret_cast<std::atomic_uint&>(*request.tail) = (tail + 1);
+        }
+        // uring response
+        {
+            unsigned const head = reinterpret_cast<std::atomic_uint&>(*response.head);
+            while(1)
+            {
+                unsigned const tail = reinterpret_cast<std::atomic_uint&>(*response.tail);
+                if(head != tail)
+                    break;
+            }
+            if(response.array[head & *response.ring_mask].res < 0) {
+                ::std::cout << response.array[head & *response.ring_mask].res << ::std::endl;
+                assert(0);
+            }
+            reinterpret_cast<std::atomic_uint&>(*response.head) = (head + 1);
+        }
+        stream->write_off += size * count;
+//        lock.release();
+        return count;
+    };
+
+
+
+
+
+}
 
 __managed__ uint32_t sum;
 __managed__ uint32_t buff[1<<18];
-__managed__ iovec vec;
 
 int main()
 {
-    io_uring_params p;
-    std::memset(&p, 0, sizeof(p));
-
-    p.flags |= IORING_SETUP_SQPOLL;
-    p.sq_thread_idle = 1000000;
-
-    int ioring_fd = __sys_io_uring_setup(ioring_size, &p);
-    if(ioring_fd < 0) {
-        std::cout << "Error in setup : " << errno << std::endl;
-        abort();
-    }
-
-    check(cudaGetLastError());
-    auto const sqptr = (__u8*)mmap(0, p.sq_off.array + p.sq_entries * sizeof(__u32),
-                                PROT_READ|PROT_WRITE, MAP_SHARED|MAP_POPULATE,
-                                ioring_fd, IORING_OFF_SQ_RING);
-    assert(sqptr != MAP_FAILED);
-    check(cudaHostRegister(sqptr, p.sq_off.array + p.sq_entries * sizeof(__u32), cudaHostRegisterDefault));
-    app_sq_ring request = {
-        (unsigned *)(sqptr + p.sq_off.head),
-        (unsigned *)(sqptr + p.sq_off.tail),
-        (unsigned *)(sqptr + p.sq_off.ring_mask),
-        (unsigned *)(sqptr + p.sq_off.flags),
-        (unsigned *)(sqptr + p.sq_off.array)
-    };
-
-    auto const sqentriesptr = (io_uring_sqe*)mmap(0, p.sq_entries * sizeof(io_uring_sqe),
-                                PROT_READ|PROT_WRITE, MAP_SHARED|MAP_POPULATE,
-                                ioring_fd, IORING_OFF_SQES);
-    assert(sqentriesptr != MAP_FAILED);
-    check(cudaHostRegister(sqentriesptr, p.sq_entries * sizeof(io_uring_sqe), cudaHostRegisterDefault));
-
-    auto const cqptr = (__u8*)mmap(0, p.cq_off.cqes + p.cq_entries * sizeof(io_uring_cqe),
-                        PROT_READ|PROT_WRITE, MAP_SHARED|MAP_POPULATE, ioring_fd,
-                        IORING_OFF_CQ_RING);
-    assert(cqptr != MAP_FAILED);
-    check(cudaHostRegister(cqptr, p.cq_off.cqes + p.cq_entries * sizeof(io_uring_cqe), cudaHostRegisterDefault));
-    app_cq_ring response = {
-        (unsigned *)(cqptr + p.cq_off.head),
-        (unsigned *)(cqptr + p.cq_off.tail),
-        (unsigned *)(cqptr + p.cq_off.ring_mask),
-        (io_uring_cqe *)(cqptr + p.cq_off.cqes)
-    };
-
-    int _infd = open("myfile", O_RDONLY);
-    assert(_infd > 0);
-
-    int infd = __sys_io_uring_register(ioring_fd,  IORING_REGISTER_FILES, &_infd, 1);
-    assert(_infd > 0);
-
-    cuda::std::atomic_bool done = {false};
-    std::thread helper([&](){
-        while(!done) {
-            if (reinterpret_cast<cuda::std::atomic_uint&>(*request.flags) & IORING_SQ_NEED_WAKEUP)
-                __sys_io_uring_enter(ioring_fd, 1, 0, IORING_ENTER_SQ_WAKEUP, NULL);
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        }
-    });
-
-    sum = 0;
-    vec = iovec{ buff, 1<<20 };
-
-    std::memset(sqentriesptr+0, 0, sizeof(io_uring_sqe));
-    sqentriesptr[0].fd = infd;
-    sqentriesptr[0].opcode = IORING_OP_READV;
-    sqentriesptr[0].addr = (uint64_t)&vec;
-    sqentriesptr[0].len = 1;
-    sqentriesptr[0].flags = IOSQE_FIXED_FILE;
-
-    auto inner = [infd, sqentriesptr, request, response] __host__ __device__ () {
-
+    int infd0 = cuda::open("myfile", O_RDONLY);
+    int infd1 = cuda::open("/dev/tty", O_WRONLY);
+    auto inner = [infd0, infd1] __host__ __device__ () {
+        cuda::FILE file0 = { infd0, 0 , 0 };
         for(int i = 0; i < (1<<10); ++i)
         {
-            // uring request
-            {
-                sqentriesptr[0].off = i<<20;
-                unsigned const tail = reinterpret_cast<cuda::std::atomic_uint&>(*request.tail);
-                while(1)
-                {
-                    unsigned const head = reinterpret_cast<cuda::std::atomic_uint&>(*request.head);
-                    if(head != ((tail + 1) & *request.ring_mask))
-                        break;
-                }
-                request.array[tail & *request.ring_mask] = 0;
-                reinterpret_cast<cuda::std::atomic_uint&>(*request.tail) = (tail + 1);
-            }
-            // uring response
-            {
-                unsigned const head = reinterpret_cast<cuda::std::atomic_uint&>(*response.head);
-                while(1)
-                {
-                    unsigned const tail = reinterpret_cast<cuda::std::atomic_uint&>(*response.tail);
-                    if(head != tail)
-                        break;
-                }
-                if(response.array[head & *response.ring_mask].res < 0) {
-                    assert(0);
-                }
-                reinterpret_cast<cuda::std::atomic_uint&>(*response.head) = (head + 1);
-            }
-            // summation
+            cuda::fread(buff, 1, 1<<20, &file0);
             for(auto b : buff)
                 sum += b;
         }
+        cuda::FILE file1 = { infd1, 0 , 0 };
+        cuda::fwrite("It worked!\n", 1, 11, &file1);
     };
 
     sum = 0;
     inner();
     std::cout << "CPU sum: " << std::dec << sum << std::endl;
 
-    sum = 0;
-    run(inner);
-    std::cout << "GPU sum: " << std::dec << sum << std::endl;
+//    sum = 0;
+//    run(inner);
+//    std::cout << "GPU sum: " << std::dec << sum << std::endl;
 
-    done = true;
-    helper.join();
+//    check(cudaHostUnregister(sqptr));
+//    check(cudaHostUnregister(sqentriesptr));
+//    check(cudaHostUnregister(cqptr));
 
-    check(cudaHostUnregister(sqptr));
-    check(cudaHostUnregister(sqentriesptr));
-    check(cudaHostUnregister(cqptr));
-    __sys_io_uring_register(ioring_fd,  IORING_UNREGISTER_FILES, &_infd, 1);
-
-    close(infd);
-    close(ioring_fd);
-
+    cuda::close(infd0);
+    cuda::close(infd1);
     return 0;
 }
